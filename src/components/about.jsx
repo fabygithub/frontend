571d6a8221e1d8c3a1e2ee7e68cefedf05f570ef
fabian cornejo <36684138+fabygithub@users.noreply.github.com>
2023-03-04 23:13:43 -0300

    Add files via upload

---
+++ src/components/about.jsx
@@ -0,0 +1,19 @@
+import React, { Component } from 'react'
+
+
+export default class About extends Component {
+
+render() {
+  return (
+     <div> 
+
+<h2>Contact</h2>
+                <p><b>Email id:</b>xyz@gmail.com</p>
+                <p><b>Mobile no :</b>1234567890</p>
+     
+     
+     
+     </div>
+  )
+ }
+}
---
+++ src/components/education.jsx
@@ -0,0 +1,33 @@
+import React, { Component } from 'react'
+
+
+export default class About extends Component {
+
+render() {
+  return (
+     <div> 
+
+          <h2>Education</h2>
+                <table>
+                    <tr>
+                        <th>University/college  </th>
+                        <th>Passing year  </th>
+                        <th>percentage/cgpa</th>
+                    </tr>
+                    <tr>
+                        <td>xyz</td>
+                        <td>2020</td>
+                        <td>8.9</td>
+                    </tr>
+                    <tr>
+                        <td>pqr</td>
+                        <td>2018</td>
+                        <td>89%</td>
+                    </tr>
+                </table> 
+     
+     
+     </div>
+  )
+ }
+}
